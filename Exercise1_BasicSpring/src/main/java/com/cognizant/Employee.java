4db6b3bb8d7e1c7ecd71a6c98f4405ecac879c05
sindhu <sindhumaran2005@gmail.com>
2026-07-02 17:51:13 +0530

    Completed Exercise 7 - Constructor and Setter Injection

--- Exercise1_BasicSpring/src/main/java/com/cognizant/Employee.java
+++ Exercise1_BasicSpring/src/main/java/com/cognizant/Employee.java
@@ -6,27 +6,25 @@ public class Employee {
     private Address address;
     private Department department;
 
-    public Employee() {
-        System.out.println("Employee Bean Created");
-    }
-
-    public void setName(String name) {
+    // Constructor Injection
+    public Employee(String name) {
         this.name = name;
+        System.out.println("Employee Bean Created using Constructor Injection");
     }
 
+    // Setter Injection
     public void setAddress(Address address) {
         this.address = address;
     }
 
+    // Setter Injection
     public void setDepartment(Department department) {
         this.department = department;
     }
 
     public void display() {
-
         System.out.println("Employee Name : " + name);
         System.out.println("City : " + address.getCity());
         System.out.println("Department : " + department.getDepartmentName());
-
     }
 }
