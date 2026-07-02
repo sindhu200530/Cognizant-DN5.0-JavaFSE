58cd67b9c3ba1c1d74f69466f14ea4e930184397
sindhu <sindhumaran2005@gmail.com>
2026-07-02 17:49:00 +0530

    Completed Exercise 5 - Spring IoC Container

---
+++ Exercise1_BasicSpring/src/main/java/com/cognizant/Department.java
@@ -0,0 +1,14 @@
+package com.cognizant;
+
+public class Department {
+
+    private String departmentName;
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+}
--- Exercise1_BasicSpring/src/main/java/com/cognizant/Employee.java
+++ Exercise1_BasicSpring/src/main/java/com/cognizant/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
 
     private String name;
     private Address address;
+    private Department department;
 
     public Employee() {
         System.out.println("Employee Bean Created");
@@ -17,10 +18,15 @@ public class Employee {
         this.address = address;
     }
 
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
     public void display() {
 
         System.out.println("Employee Name : " + name);
         System.out.println("City : " + address.getCity());
+        System.out.println("Department : " + department.getDepartmentName());
 
     }
 }
