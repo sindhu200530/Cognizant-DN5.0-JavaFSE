beca77bab2dbb3effc660d100ffd372c5e0f9543
sindhu <sindhumaran2005@gmail.com>
2026-07-02 18:10:11 +0530

    Completed Exercise 9 - Spring Boot Application

---
+++ Exercise9_SpringBoot/src/main/java/com/cognizant/Exercise9SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise9SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise9SpringBootApplication.class, args);
+    }
+
+}
